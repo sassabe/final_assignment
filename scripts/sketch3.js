b7cf7fc57d6769ecff26bb1ce4b6048d20493429
sassabe <donadio.isabella@gmail.com>
2018-05-05 15:04:16 -0400

    changes to images

--- scripts/sketch3.js
+++ scripts/sketch3.js
@@ -1,4 +1,10 @@
-
+var smallTree;
+var bigTree;
+var runningHorses;
+var grazingHorse;
+var theta = 0;
+let x = 0;
+let speed = 1;
 
 function setup() {
     let canvas = createCanvas(windowWidth, windowHeight);
@@ -10,31 +16,45 @@ function setup() {
     bigTree =loadImage("assets/Green_Realistic_Tree_PNG_Clip_Art-1097.png");
     runningHorses = loadImage("assets/horses.png");
     grazingHorse = loadImage("assets/grazingHorse.png");
+    cloud = loadImage("assets/cloud.png");
+
+
 
 }
 
 function draw() {
 
+
+
     //sky
-    noStroke();
-    fill(61, 91, 140);
-    rect(0,-0,windowWidth,windowHeight/2);
+   noStroke();
+   fill(61, 91, 140);
+   rect(0,-0,windowWidth,windowHeight/2);
 
+   //moving clouds
+   if (x >= width || x < 0) {
+       speed = -speed;
+     }
+     fill(252, 248, 224);
+     image(cloud, x, height / 3, cloud.width/5, cloud.height/5);
+     x = x + speed;
 
-    //moon
-    noStroke();
-    fill(252, 248, 224);
-    ellipse(width/6,height/5,60,60);
+    // moon
 
-    //trees and horses
-    image(smallTree, 0, height/3, img.width/6, img.height/6);
-    image(smallTree, 100, height/3, img.width/6, img.height/6);
-    image(smallTree, 300, height/3, img.width/6, img.height/6);
-    image(runningHorses, mouseX, mouseY, img.width/6, img.height/6);
-    image(bigTree, 700, height/3, img.width/6, img.height/6);
-    image(smallTree, 800, height/3, img.width/6, img.height/6);
-    image(bigTree, 950, height/3, img.width/6, img.height/6);
-    image(grazingHorse, mouseX, mouseY, img.width/6, img.height/6);
+      noStroke();
+      fill(252, 248, 224);
+      ellipse(width/6,height/5,60,60);
+
+    //
+    // //trees and horses
+    image(smallTree, 0, height/3, smallTree.width/6, smallTree.height/6);
+    image(smallTree, 100, height/3, smallTree.width/6, smallTree.height/6);
+    image(smallTree, 300, height/3, smallTree.width/6, smallTree.height/6);
+    image(runningHorses, mouseX, mouseY, runningHorses.width/6, runningHorses.height/6);
+    image(bigTree, 700, height/3, bigTree.width/6, bigTree.height/6);
+    image(smallTree, 800, height/3, smallTree.width/6, smallTree.height/6);
+    image(bigTree, 950, height/3, bigTree.width/6, bigTree.height/6);
+    image(grazingHorse, mouseX, mouseY, grazingHorse.width/6, grazingHorse.height/6);
 
 
   }
